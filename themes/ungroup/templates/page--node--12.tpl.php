b5636d0eadd0e51537b0f2032085f9332b316cda
shayank <shayan.rgb@gmail.com>
2013-11-06 16:22:40 +0330

    fix footer text

--- themes/ungroup/templates/page--node--12.tpl.php
+++ themes/ungroup/templates/page--node--12.tpl.php
@@ -109,6 +109,6 @@
 	  	 | <a href="/contact-us">contact</a>
 	  	 | <a href="/about-us">about us</a>
 	  	</p>
-  		<p class="company">&copy; Company 2012</p>
+  		<p class="company">copyright &copy; 2013 Un group. All right reserved. Powered by Azgil Software.</p>
   	  </div>
   </div>
